a161c9917ef1e9a2c3c36591a2949faebb13dd7a
Justin Lewis Salmon <justin2.salmon@live.uwe.ac.uk>
2014-11-01 01:39:37 +0100

    Use GPIO trigger for ADC read op

--- sensors/adc/adc.py
+++ sensors/adc/adc.py
@@ -1,9 +1,21 @@
 from abc import abstractmethod
 import random
+import sys
 import threading
-from time import sleep
+from time import time
 from spidev import spidev
 from util.logger import Logger
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print('Error importing RPi.GPIO!'
+          ' This is probably because you need superuser privileges.'
+          ' You can achieve this by using \'sudo\' to run your script')
+    sys.exit(1)
+except ImportError:
+    print('Be sure to run this on a Raspberry Pi B+ ;)')
+    print('Continuing anyway ...')
 
 log = Logger(__name__).setup()
 
@@ -14,26 +26,50 @@ class ADC(threading.Thread):
     """
     def __init__(self, channel, listener):
         super(ADC, self).__init__()
-        log.info('Initialising SPI interface')
 
         self.channel = channel
         self.listener = listener
 
-        # Set up SPI interface
-        self.spi = spidev.SpiDev()
-        self.spi.open(0, channel)
+        # GPIO trigger interrupt pin
+        self.TRIGGER = 22
 
-    def run(self):
+        # Set up SPI interface
+        self.init_spi()
+
+        # Set up GPIO interrupt
+        self.init_gpio()
+
+    def init_spi(self):
+        log.info('Initialising SPI interface')
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, self.channel)
+
+    def init_gpio(self):
+        log.info('Initialising GPIO trigger')
+        GPIO.setmode(GPIO.BOARD)
+        # Set up falling edge detection on the trigger pin
+        GPIO.setup(self.TRIGGER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.TRIGGER, GPIO.FALLING,
+                              callback=self.on_trigger,
+                              bouncetime=20)
+
+    def on_trigger(self, channel):
         """
-        Read data continuously from the sensor.
+        Called when the trigger pin is pulled low. Immediately read data from
+        the sensor.
         """
-        while True:
-            # Read/write some dummy data for now
-            response = self.spi.xfer2([random.randint(0, 35),
-                                       random.randint(0, 35)])
-            # Invoke the callback listener
-            self.listener.on_event(response)
-            sleep(1)
+        start = time()
+        # Read/write some dummy data for now
+        response = self.spi.xfer2([random.randint(0, 35),
+                                   random.randint(0, 35)])
+        end = time()
+        log.debug("Read event data in " + str((end - start) * 1000) + "ms")
+
+        # Invoke the callback listener
+        self.listener.on_event(response)
+
+    def cleanup(self):
+        GPIO.cleanup()
 
 
 class EventListener(object):
